6ece340759e02a6435c620c98472fb6254428f71
suryasardar <suryaprakashchary515@gmail.com>
2024-03-13 11:37:46 +0530

    search done

--- Frontend/my-app/src/compoents/ navbar.jsx
+++ Frontend/my-app/src/compoents/ navbar.jsx
@@ -1,9 +1,7 @@
-
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-import Blog from "../Images/logo.png"
+import Blog from "../Images/logo.png";
 // import { DarkModeSwitch } from 'react-toggle-dark-mode';
- 
 
 const Navbar = () => {
   const [logged, setLogged] = useState(false);
@@ -22,14 +20,29 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <Link to="/" className=" w-10"  >
-     <img src={Blog} alt="BLOG"/>
+      <Link to="/" className=" w-10">
+        <img src={Blog} alt="BLOG" />
       </Link>
+      <div className="absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 
+      px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto">
+        
+
+        <input
+          type="text"
+          placeholder="search"
+          className="w-full md:w-auto bg-grey p-2 pl-6  pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12"
+          />
+        <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md-left-5 top-1/2 -translate-y-1/2 text:xl text-dark-grey"></i>
+          
+      </div>
+      <div className="flex items-center gap-3 md:gap-6 ml-auto">
+        <button className="md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center">
+        <i className="fi fi-rr-search text-xl"></i>
+        </button>
+      </div>
       <ul>
         <li>
-          <Link to="/blog">
-            CreateBlog
-          </Link>
+          <Link to="/blog">CreateBlog</Link>
         </li>
         {logged ? (
           <li>
@@ -49,9 +62,7 @@ const Navbar = () => {
           </li>
         ) : (
           <li>
-            <Link to="/login">
-              Sign In
-            </Link>
+            <Link to="/login">Sign In</Link>
           </li>
         )}
       </ul>
