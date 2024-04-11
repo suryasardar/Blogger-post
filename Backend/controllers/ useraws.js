93532de0354fa9e7814b4f8975bcb49f9eaf6e92
suryasardar <suryaprakashchary515@gmail.com>
2024-04-11 18:53:21 +0530

    donew

--- Backend/controllers/ useraws.js
+++ Backend/controllers/ useraws.js
@@ -1,31 +1,47 @@
 const aws = require('aws-sdk');
-const uid = require("uuid");
+const express = require('express');
+const uuid = require("uuid");
 
 
 const s3 = new aws.S3({
     region: 'ap-southeast-2',
-    accessKeyId:process.env.accessKeyId,
-    secretAccessKey:process.env.secretAccessKey,
+    credentials: new aws.Credentials({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    })
 
 })
 
 const generateURL = async () => {
     const date = new Date();
-    const imagename = `${uid()}-${date.getTime()}.jpg`;
-c 
-   return await s3.getSignedUrlPromise('putObject', {
-        Bucket: 'blog-website-op',
-        Key: imagename,
-        Expires: 1000,
-        ContentType:"image/jpg"
-    })
+    const imagename = `${date.getTime()}.jpg`;
+
+    try {
+        const params = {
+            Bucket: 'blog-website-op',
+            Key: imagename,
+            Expires: 1000,
+            ContentType: "image/jpg"
+        };
+        const url = await s3.getSignedUrlPromise('putObject', params);
+        console.log(url,"data");
+        return url;
+    } catch (error) {
+        console.error("Error generating URL:", error);
+        throw error; // Propagate error to caller
+    }
 }
+
+
+ 
+       
 
 const AWS = ('/get-upload-url', (req, res) => {
     generateURL().then(url => {
         res.status(200).json({ uploadurl: url });
     }).catch(err => {
-        return res.status(500).json({ error: err.message });
+        console.log(err,"errrr")
+        return res.status(500).json({ error: "not done" });
     })
 })
 
